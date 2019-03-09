303004c8ec77e8fd6e35d431c80bbab157283a1c
Mark Barrett <mark.barrett3@mydit.ie>
2019-03-09 12:19:07 +0000

    Created main.c file

---
+++ main.c
@@ -0,0 +1,12 @@
+/* Main File for the daemon. This file is responsible for choreographing the main aspects of the application. Initially it will start as a daemon
+ * and will control other processes via message queues where appropriate. It will call processes for running backups and logs etc. More info
+ * here as the program develops.
+ * Author: Mark Barrett
+ * Date: 09/03/2019
+ * Git Repo: https://github.com/mark-barrett/Code-Backup-and-Deployment-Daemon
+ */
+#include <stdio.h>
+
+int main() {
+	return 0;
+}
